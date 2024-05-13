ba9f7be2fe808953e5400cca0b8ebfde32552f42
RcCreeperTech <devinprothero@gmail.com>
2024-05-13 14:15:01 -0700

    change tab settings

--- init.lua
+++ init.lua
@@ -139,6 +139,9 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
